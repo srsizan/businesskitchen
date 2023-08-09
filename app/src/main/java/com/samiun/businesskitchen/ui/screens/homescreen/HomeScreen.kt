323ac4ba93b2963ba91662b93a75c7b8648f27c9
Samiun Sizan <samiunsizan@gmail.com>
2023-08-09 17:29:40 +0600

    [Business kitchen] Items added to list

--- app/src/main/java/com/samiun/businesskitchen/ui/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/com/samiun/businesskitchen/ui/screens/homescreen/HomeScreen.kt
@@ -2,11 +2,16 @@ package com.samiun.businesskitchen.ui.screens.homescreen
 
 import android.app.Activity
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
-import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -18,7 +23,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,7 +33,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -39,7 +42,6 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.samiun.businesskitchen.R
 import com.samiun.businesskitchen.ui.components.HomeScreenTopBar
-import com.samiun.businesskitchen.ui.screens.Screen
 import com.samiun.businesskitchen.ui.screens.SharedViewModel
 import com.samiun.businesskitchen.ui.screens.homescreen.HomeScreenContants.CAN_ACCESS
 import com.samiun.businesskitchen.ui.screens.homescreen.HomeScreenContants.USER_ID
